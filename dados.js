a29fb941b463155efdb9d133da03ba69af0b3432
Cristiane Justino <85750532+crisjustinno@users.noreply.github.com>
2024-09-07 11:42:25 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,73 @@
+let dados = [
+    {
+        titulo : "Isaac Asimov",
+        descricao : "Nascido na Rússia e naturalizado americano, Asimov era um prolífico escritor de ficção científica e um respeitado divulgador científico. Conhecido por suas Leis da Robótica, ele explorou temas como inteligência artificial, ética e o futuro da humanidade.",
+        principaisObras : "Série Fundação, Eu, Robô, O Fim da Eternidade",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Isaac_Asimov",
+        tags: "fundação robôs robô leis robótica"
+    },
+    {
+        titulo: "Ray Bradbury",
+        descricao: "Escritor e poeta americano, Bradbury é famoso por suas histórias curtas e romances que exploram a natureza humana e o impacto da tecnologia na sociedade.",
+        principaisObras: "Fahrenheit 451, Crônicas Marcianas, O Vinho do Espinho.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Ray_Bradbury",
+        tags: "farenheit 451 crônicas marcianas"
+    },
+    {
+        titulo: "Arthur C. Clarke",
+        descricao: "Cientista e escritor britânico, Clarke foi um dos maiores expoentes da ficção científica hard, baseada em conceitos científicos sólidos.",
+        principaisObras: "2001: Uma Odisseia no Espaço, Encontro com Rama",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Arthur_C._ClarkeB",
+        tags: "2001 odisséia infância alienígenas"
+    },
+    {
+        titulo: "H.G. Wells",
+        descricao: "Escritor inglês, considerado um dos pioneiros da ficção científica, Wells explorou temas como viagens no tempo, invasões alienígenas e engenharia social.",
+        principaisObras: "A Máquina do Tempo, A Guerra dos Mundos, O Homem Invisível",
+        linkBiografia: "https://pt.wikipedia.org/wiki/H._G._Wells",
+        tags: "guerra dos mundos invisível"
+    },
+    {
+        titulo: "Jules Verne",
+        descricao: "Foi um escritor francês que revolucionou a literatura com suas histórias de aventura e ficção científica. Nascido em 1828, Verne é considerado por muitos o pai da ficção científica, pois foi um dos primeiros autores a explorar temas como viagens espaciais, submarinos e a exploração de mundos desconhecidos.",
+        principaisObras: "Vinte Mil Léguas Submarinas, Da Terra à Lua, A Volta ao Mundo em 80 Dias.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Jules_Verne",
+        tags: "80 dias vinte 20 mil léguas"
+    },
+    {
+        titulo: "Philip K. Dick",
+        descricao: "Escritor americano, Dick é famoso por suas histórias complexas e perturbadoras, que exploram temas como identidade, realidade e a natureza da consciência.",
+        principaisObras: "O homem do castelo alto, Ubik",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Philip_K._Dick",
+        tags: "homem castelo alto"
+    },
+    {
+        titulo: "Aldous Huxley",
+        descricao: "Escritor inglês, Huxley é conhecido por suas distopias que exploram os perigos da sociedade tecnológica e a manipulação genética.",
+        principaisObras: "Admirável Mundo Novo.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Aldous_Huxley",
+        tags: "admirável mundo novo"
+    },
+    {
+        titulo: "Ursula K. Le Guin",
+        descricao: "Escritora americana, Le Guin é famosa por suas obras de fantasia e ficção científica que exploram temas como gênero, cultura e política.",
+        principaisObras: "A Mão Esquerda da Escuridão, Terramar.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Ursula_K._Le_Guin",
+        tags: "mão esquerda escuridão despossuídos"
+    },
+    {
+        titulo: "Robert A. Heinlein",
+        descricao: "Foi um dos mais influentes escritores de ficção científica do século XX. Nascido nos Estados Unidos, ele combinava uma mente científica com uma imaginação fértil, criando histórias que exploravam os limites da tecnologia, da sociedade e da condição humana.",
+        principaisObras: "Estranho em Terra Estranha, O Homem que Vendeu a Lua.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Robert_A._Heinlein",
+        tags: "estranho terra estranha"
+    },
+    {
+        titulo: "Margaret Atwood",
+        descricao: "Escritora canadense, Atwood é uma das autoras mais importantes da ficção científica contemporânea, conhecida por suas distopias feministas.",
+        principaisObras: "O Conto da Aia, O Devorador.",
+        linkBiografia: "https://pt.wikipedia.org/wiki/Margaret_Atwood",
+        tags: "conto aia"
+    }
+];
+
